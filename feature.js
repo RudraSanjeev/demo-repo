4fdc1847b28789623eb5bde126298471d7ea7848
sanjeev <sanjeev@rudrainnovative.in>
2023-10-09 13:25:20 +0530

    one new feature added in master

--- feature.js
+++ feature.js
@@ -1,6 +1,4 @@
 console.log("this is feature file added");
-<<<<<<< HEAD
 console.log("some more feature is added in this features");
-=======
 console.log("feature in the feature branch is added to more.");
->>>>>>> feature
+console.log("this is new feature added inside master branch");
